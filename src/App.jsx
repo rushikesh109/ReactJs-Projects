24e45a442b139769b892dd0e1cf518f83f74dde8
rushikesh109 <mangrulerushikesh2003@gmail.com>
2026-01-12 23:26:05 +0530

    feat: learn about components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import FirstDiv from "./components/FirstDiv";
+import Example from "./components/Hero";
+import SecondDiv from "./components/SecondDiv";
+
+const App = () => {
+  return (
+    <>
+    <Example/>
+      <FirstDiv />
+      <SecondDiv />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/SecondDiv.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SecondDiv = () => {
+  return (
+    <div>
+      <div className='card'>
+      <h1 >Hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, enim, facere a est rem aspernatur maxime ratione laborum magnam aperiam nostrum aliquam cumque! Quisquam ea consequatur enim labore fugit asperiores.</p>
+    </div>
+    </div>
+  )
+}
+
+export default SecondDiv
