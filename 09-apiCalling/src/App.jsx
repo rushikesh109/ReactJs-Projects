2a3c12619e7d4df1400dffd87a4a53bff0537f3f
rushikesh109 <mangrulerushikesh2003@gmail.com>
2026-01-17 19:46:48 +0530

    feat: api calling axios and fetch

--- 09-apiCalling/src/App.jsx
+++ 09-apiCalling/src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
+// ----------------------- USING FETCH METHOD---------------------------------
+//  async function apiReq () {
+//     const data =  await fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     console.log(data);
+//   }
+
+// const apiReq = async() => {
+//   const data = await fetch('https://jsonplaceholder.typicode.com/todos/')
+
+//   const js = await data.json()
+//   console.log(js);
+  
+// }
+
+
+
+// ----------------------- USING AXIOS METHOD---------------------------------
+
+// const apiReq  =  async() => {
+//  const response =  await axios.get('https://jsonplaceholder.typicode.com/todos/users')
+// console.log(response.data); 
+// }
+
+const [data , setData] = useState([])
+
+const apiReq = async() => {
+  const response = await axios.get('https://picsum.photos/v2/list')
+ setData(response.data)
+  
+}
   return (
-    <>
+    <div>
+      <button onClick={apiReq}>GetData</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data.map(function(elem, idx) {
+          return <h3>Hello {elem.author} {idx}</h3>
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
